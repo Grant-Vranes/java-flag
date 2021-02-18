8794fb5cbc77a0cdf655868fe5ef9cbe66afbee3
刘瑜澄 <481158831@qq.com>
2021-02-18 22:46:29 +0800

    2021.2.18

---
+++ Code/PartOne/src/Y2021M2D18/HelloWorld_1.java
@@ -0,0 +1,13 @@
+package Y2021M2D18;//声明包
+
+/**
+ * @author Grant·Vranes
+ *
+ */
+public class HelloWorld_1 {//声明类
+	//主方法main，为程序的入口
+	//程序的执行从main开始，main结束则程序结束
+	public static void main(String[] args) {
+		System.out.println("Hello , 2021.2.18");
+	}
+}
