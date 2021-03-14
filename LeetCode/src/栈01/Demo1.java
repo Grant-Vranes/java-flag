1222727bf1032ff86f208ad454992289b32a4851
刘瑜澄 <481158831@qq.com>
2021-03-14 23:10:42 +0800

    2021.3.14

---
+++ LeetCode/src/栈01/Demo1.java
@@ -0,0 +1,70 @@
+package 栈01;
+
+import java.util.Stack;
+
+/**例1：
+ * 字符串中只有字符'('和')'，合法字符串需要括号可以配对，比如：(),(())
+ * 像((,)),)( 等等都是不合法的
+ */
+public class Demo1 {
+
+    public static void main(String[] args) {
+        Demo1 o = new Demo1();
+        System.out.println(o.isValid("()()"));
+    }
+
+    public boolean isValid(String s){
+    //当字符串本来就是空的时候，我们可以快速返回true
+        if(s==null || s.length()==0){
+        return true;
+    }
+    //当字符串长度为奇数时，不可能是一个有效的合法字符串
+        if(s.length()%2 == 1){
+        return false;
+    }
+    //消除法的主要核心逻辑
+        Stack<Character> t = new Stack<Character>();
+        for(int i=0; i<s.length(); i++){
+            char c = s.charAt(i);
+            if(c=='('){//压栈
+                t.push(c);
+            }else if(c==')'){//弹栈
+                if(t.empty()){
+                    return false;
+                }
+                t.pop();
+            }
+
+        }
+        return t.empty();
+    }
+
+    /**深度优化解法：栈中存放内容一样，并且有消除特性，我们可以使用leftBraceNumber的加和减来代替入栈和出栈
+    public boolean isValid(String s){
+        //当字符串本来就是空的时候，我们可以快速返回true
+        if(s==null || s.length()==0){
+            return true;
+        }
+        //当字符串长度为奇数时，不可能是一个有效的合法字符串
+        if(s.length()%2 == 1){
+            return false;
+        }
+        //消除法的主要核心逻辑
+        int leftBraceNumber = 0;
+        for(int i=0; i<s.length(); i++){
+            //取出字符
+            char c = s.charAt(i);
+            if(c=='('){//压栈
+                leftBraceNumber++;
+            }
+            if(c==')'){//弹栈
+                if(leftBraceNumber<=0){
+                    return false;//如果弹栈失败
+                }
+                --leftBraceNumber;
+            }
+        }
+        return leftBraceNumber==0;//
+    }
+     */
+}
---
+++ LeetCode/src/栈01/Demo2.java
@@ -0,0 +1,59 @@
+package 栈01;
+
+import java.util.Stack;
+
+/**
+ * 广度扩展普适性:
+ * 	给定一个只包括'(',')','{','}','[',']'的字符串，判断字符串是否有效。有效字符串需满足：
+ *         1)左括号必须用相同类型的右括号闭合
+ *         2)左括号必须以正确的顺序闭合
+ *         3)注意空字符串可被认为是有效字符串
+ */
+
+public class Demo2 {
+    public static void main(String[] args) {
+        Demo2 o = new Demo2();
+        System.out.println(o.isValid("({[]})"));
+    }
+
+    public boolean isValid(String s){
+        //当字符串为空时，认为是有效字符串
+        if(s.length()==0 || s.isEmpty() || s==null){
+            return true;
+        }
+        //如果字符串奇数个，不可能是一个有效的合法字符串
+        if(s.length()%2==1){
+            return false;
+        }
+        //核心逻辑
+        Stack<Character> t = new Stack<Character>();
+        for (int i = 0; i < s.length(); i++) {
+            char c= s.charAt(i);
+            if(c=='(') {
+                t.push(c);
+            }else if(c==')'){
+                if(t.empty()){
+                    return false;
+                }
+                t.pop();
+            }
+            if(c=='{') {
+                t.push(c);
+            }else if(c=='}'){
+                if(t.empty()){
+                    return false;
+                }
+                t.pop();
+            }
+            if(c=='[') {
+                t.push(c);
+            }else if(c==']'){
+                if(t.empty()){
+                    return false;
+                }
+                t.pop();
+            }
+        }
+        return t.empty();
+    }
+}
